7a5889caf234fa6aef0ed6222c912c7894ae6408
frapoleon <wysfrapoleon@126.com>
2016-03-27 23:08:13 -0400

    update

--- shiny code/global.R
+++ shiny code/global.R
@@ -11,8 +11,24 @@ library(maptools)
 library(ggmap)
 
 ####################################### compalin
-data_2015_top5 <- readRDS("data_2015_top5_complain.RDS")
+data_2015_top5 <- read.csv("complain data(1,4,7,10).csv")
+data_2015_top5 <- data_2015_top5[ , 2:10]
+####################################### Crime
+crime <- read.csv("NYPD_7_Major_Felony_Incidents.csv", header = T)
+crime <- crime[which(crime$Occurrence.Year == 2015), ]
+crime <- na.omit(crime)
+crime <- crime[ , c("Occurrence.Date", "Day.of.Week", "Occurrence.Month", "Occurrence.Day", "Occurrence.Year", "Occurrence.Hour", "Offense", "Borough", "XCoordinate", "YCoordinate", "Location.1")]
 
+location <- unlist(strsplit(as.character(crime$Location.1), split=c(", ")))
+index1 <- seq(1, length(location), 2)
+index2 <- seq(2, length(location), 2)
+crime$Lat <- location[index1]
+crime$Lat <- unlist(strsplit(crime$Lat, "\\("))[index2]
+crime$Long <- location[index2]
+crime$Long <- unlist(strsplit(crime$Long, "\\)"))
+options(digits=15)
+crime$Lat <- as.numeric(crime$Lat)
+crime$Long <- as.numeric(crime$Long)
 
 ####################################### housing price
 home_value = read.csv("home_value.csv")
